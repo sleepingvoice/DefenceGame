7b2dfa69198bd59c0326a43acc03d13b30948f83
GuHyunWoo <rkwhr99@gmail.com>
2023-08-29 17:25:04 +0900

    맵 기능 추가

---
+++ Assets/04.Scripts/Area/AreaObjActive.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaObjActive : MonoBehaviour
+{
+	public List<GameProgress> ShowProgress;
+	private bool _activeCheck = false;
+
+	private void Awake()
+	{
+
+		MainGameData.s_progressValue.AddListener((value) =>
+		{
+			_activeCheck = false;
+			foreach (var progress in ShowProgress)
+			{
+				if (value == progress)
+					_activeCheck = true;
+			}
+			if (_activeCheck)
+				this.gameObject.SetActive(true);
+			else
+				this.gameObject.SetActive(false);
+		});
+	}
+}
